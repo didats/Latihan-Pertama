405271c4fd056c2094f1060f0d17d6f955a09fef
Didats Triadi <didats@gmail.com>
2016-02-09 16:28:26 +0700

    Baca file dan filter yang kosong

--- pertama.php
+++ pertama.php
@@ -1,2 +1,25 @@
 <?php
+	$arrFile = array("ADL.txt", "AG.txt", "GA.txt");
 	
+	function bacaFile($namafile) {
+		
+		global $dir;
+		
+		$file = fopen(getcwd()."/".$namafile, "r");
+		$baris = fgets($file);
+		
+		while(($baris = fgets($file)) ==! FALSE) {
+			
+			$data = explode(",", $baris);
+			
+			if (count($data) > 3) {
+				$strAngkot = $data[1];
+				echo $strAngkot."<br />";
+			}
+			
+		}
+	}
+	
+	for ($i=0; $i<count($arrFile); $i++) {
+		bacaFile($arrFile[$i]);
+	}
