1448c6755a91b08c404297ccfc99fec2f67f7f36
Didats Triadi <didats@gmail.com>
2016-02-09 17:22:30 +0700

    Koneksi database belum berhasil

--- pertama.php
+++ pertama.php
@@ -1,9 +1,15 @@
 <?php
+	error_reporting(E_ALL); 
+	ini_set('display_errors', 1);
+	
+	// koneksi ke database
+	$mysqli = new mysqli("localhost", "root", "root", "transiti");
+	
 	$arrFile = array("ADL.txt", "AG.txt", "GA.txt");
 	
 	function bacaFile($namafile) {
 		
-		global $dir;
+		global $dir, $mysqli;
 		
 		$file = fopen(getcwd()."/".$namafile, "r");
 		$baris = fgets($file);
@@ -13,8 +19,25 @@
 			$data = explode(",", $baris);
 			
 			if (count($data) > 3) {
-				$strAngkot = $data[1];
+				$strAngkot = $data[0];
+				
+				$strQuery = "INSERT INTO angkot SET angkot_name = '$strAngkot', angkot_desc='$strAngkot', angkot_price='4000'";
+				echo $strQuery."<br />";
+				
+				
+				/*
+				$strSelect = "SELECT * FROM angkot WHERE angkot_name = '$strAngkot'";
+				$rst = $mysqli->query($strSelect);
+				$rows = $rst->fetch_array();
+				
+				print_r($rows);
+				exit;
+				
+				
+				$strQuery = "INSERT INTO angkot SET angkot_name = '$strAngkot'";
+				
 				echo $strAngkot."<br />";
+				*/
 			}
 			
 		}
