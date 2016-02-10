e92cbb9e7f7b4a75288d8a6f35317095189859cd
Didats Triadi <didats@gmail.com>
2016-02-10 16:45:07 +0700

    API Test

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+	/*
+	{
+		'merk': {
+			'item': [
+				{'name': "Panasonic", 'country': "Japan"},
+				{'name': "Samsung", 'country': "South Korea"},
+			]
+		}
+	}
+	
+	*/
+	
+	$arrData = array('0' => 1, '1' => 2, '2' => 3);
+	$arr = array('test' => 5, 'satu' => 10);
+	
+	$arr['test']
+	$arrData[0];
+	
+	
+	$data = array(
+		'merk' => array(
+			'item' => array(
+				array('name' => "Panasonic", 'country' => "Japan"),
+				array('name' => "Samsung", 'country' => "South Korea"),
+			)
+		)
+	);
