c201c107410b345bf7a4dc196275f3e059d566a8
Didats Triadi <didats@gmail.com>
2016-02-10 17:09:32 +0700

    API json

--- api.php
+++ api.php
@@ -1,28 +1,11 @@
 <?php
-	/*
-	{
-		'merk': {
-			'item': [
-				{'name': "Panasonic", 'country': "Japan"},
-				{'name': "Samsung", 'country': "South Korea"},
-			]
-		}
-	}
+	header("Content-type: application/json");
 	
-	*/
-	
-	$arrData = array('0' => 1, '1' => 2, '2' => 3);
-	$arr = array('test' => 5, 'satu' => 10);
-	
-	$arr['test']
-	$arrData[0];
-	
-	
-	$data = array(
-		'merk' => array(
-			'item' => array(
-				array('name' => "Panasonic", 'country' => "Japan"),
-				array('name' => "Samsung", 'country' => "South Korea"),
-			)
+	$arr = array(
+		'data' => array(
+			array('name' => "Panasonic"),
+			array('name' => "Samsung"),
 		)
 	);
+	
+	echo json_encode($arr);
