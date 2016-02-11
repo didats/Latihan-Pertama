1889d72ecf5ab5256f18196bb63a4894b85a4c36
Didats Triadi <didats@gmail.com>
2016-02-11 16:52:29 +0700

    API Angkot

--- api.php
+++ api.php
@@ -1,11 +1,18 @@
 <?php
 	header("Content-type: application/json");
 	
-	$arr = array(
-		'data' => array(
-			array('name' => "Panasonic"),
-			array('name' => "Samsung"),
-		)
-	);
+	// koneksi
+	$mysqli = new mysqli("localhost", "root", "root", "transiti");
+	$rst = $mysqli->query("SELECT * FROM angkot");
+	
+	$arr = array();
+	while($row = $rst->fetch_array()) {
+		array_push($arr, array(
+			'id' => $row[0],
+			'name' => $row[1],
+			'desc' => $row[2],
+			'price' => $row[3]
+		));
+	}
 	
 	echo json_encode($arr);
